2a463d500b323385fd9406a70fc0df8dcc8e30fa
RadevaPutra <radevaputra@example.com>
2026-06-08 22:05:36 +0700

    Add Vercel build configuration

---
+++ vercel_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script to build Flutter web on Vercel
+
+echo "Installing Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Checking Flutter version..."
+flutter --version
+
+echo "Enabling web..."
+flutter config --enable-web
+
+echo "Getting dependencies..."
+flutter pub get
+
+echo "Building Flutter Web..."
+flutter build web --release
+
+echo "Build process completed!"
